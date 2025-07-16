0cdb206efec5f528b45f606f00f22934a4aeb1eb
spatriciade <spatriciade@hotmail.com>
2025-07-16 19:30:30 +0200

    more about TS

--- JS/app.js
+++ JS/app.js
@@ -22,3 +22,86 @@ hero = {
     character: "Batman",
     powers: ["Intelligence", "Wealth", "Martial Arts"],
 };
+const Hulk = {
+    character: "Bruce Banner",
+    powers: ["Intelligence", "Wealth", "Martial Arts"],
+};
+const series1 = {
+    title: "From",
+    mainCharacter: "Monsters",
+    seasons: 3,
+};
+const series2 = {
+    title: "Games of Thrones",
+    mainCharacter: "Dragons",
+    seasons: 5,
+};
+let user1 = {
+    user: "Jhon",
+    password: "abcd",
+};
+let user2 = {
+    user: "Jhon",
+    password: 1234,
+};
+//Tipos enumerados
+var Sizes;
+(function (Sizes) {
+    Sizes[Sizes["SMALL"] = 0] = "SMALL";
+    Sizes[Sizes["MEDIUM"] = 1] = "MEDIUM";
+    Sizes[Sizes["LARGE"] = 2] = "LARGE";
+    Sizes[Sizes["X_LARGE"] = 3] = "X_LARGE";
+})(Sizes || (Sizes = {}));
+let pulloverSizes = Sizes.MEDIUM;
+//Tipado dinámico
+let password = "abcd";
+password = 1234;
+password = [];
+let password2 = true;
+password2 = 1234;
+//let password3 = password2+123
+let password3 = password2 + "true";
+console.log(typeof password3);
+console.log(password3);
+let word;
+word = 1234;
+word = "abcd";
+let animal = {
+    weight: 200,
+    color: "white",
+};
+let animal2;
+animal2 = {
+    weight: 250,
+    color: "brown",
+};
+animal2 = {
+    weight: 150,
+    speed: 50,
+    color: "pink",
+};
+let animal3;
+animal3 = {
+    weight: 300,
+    color: "brown",
+    speed: 150,
+    strenght: "max"
+};
+hello = undefined;
+console.log(typeof hello);
+hello = null;
+console.log(typeof hello);
+console.log(hello);
+const sayHello2 = (greet = "Patricia") => {
+    console.log("Hello" + greet);
+};
+sayHello2();
+sayHello2("Silvia");
+const retornoInancalzable = () => {
+    throw TypeError("Sourcing went wrone");
+};
+const retornoInancalzable2 = (number) => {
+    while (true) {
+        console.log(number++);
+    }
+};
--- JS/app.ts
+++ JS/app.ts
@@ -30,3 +30,138 @@ hero = {
   character: "Batman",
   powers: ["Intelligence", "Wealth", "Martial Arts"],
 };
+
+//interfaces
+
+interface heroInterface {
+  character: string;
+  powers: string[];
+}
+
+const Hulk: heroInterface = {
+  character: "Bruce Banner",
+  powers: ["Intelligence", "Wealth", "Martial Arts"],
+};
+
+//Alias de tipo
+
+type seriesType = {
+  title: string;
+  mainCharacter: string;
+  seasons: number;
+};
+
+const series1: seriesType = {
+  title: "From",
+  mainCharacter: "Monsters",
+  seasons: 3,
+};
+
+const series2: seriesType = {
+  title: "Games of Thrones",
+  mainCharacter: "Dragons",
+  seasons: 5,
+};
+
+//interfaces como tipos
+interface Login<U, P> {
+  user: U;
+  password: P;
+}
+let user1: Login<string, string> = {
+  user: "Jhon",
+  password: "abcd",
+};
+let user2: Login<string, number> = {
+  user: "Jhon",
+  password: 1234,
+};
+
+//Tipos enumerados
+
+enum Sizes {
+  SMALL,
+  MEDIUM,
+  LARGE,
+  X_LARGE,
+}
+let pulloverSizes = Sizes.MEDIUM;
+
+//Tipado dinámico
+
+let password: any = "abcd";
+password = 1234;
+password = [];
+
+let password2: unknown = true;
+password2 = 1234;
+//let password3 = password2+123
+let password3 = password2 + "true";
+console.log(typeof password3);
+console.log(password3);
+
+//Uniones, intersecciones,tipos literales
+
+interface Bear {
+  weight: number;
+  color: string;
+}
+interface Tyger {
+  weight: number;
+  speed: number;
+}
+interface Lion{
+  strenght:string;
+  speed:number;
+
+}
+
+let word: number | string;
+word = 1234;
+word = "abcd";
+
+let animal: Bear = {
+  weight: 200,
+  color: "white",
+};
+let animal2: Bear | Tyger;
+animal2 = {
+  weight: 250,
+  color: "brown",
+};
+
+animal2 = {
+  weight: 150,
+  speed: 50,
+  color: "pink",
+};
+ let animal3 : Bear |(Tyger & Lion)
+
+ animal3 = {
+  weight:300,
+  color:"brown",
+  speed:150,
+  strenght:"max"
+ }
+
+ hello = undefined;
+ console.log(typeof hello);
+ hello = null;
+ console.log(typeof hello);
+ console.log(hello);
+
+ const sayHello2 = (greet:string = "Patricia"):void => {
+  console.log("Hello"+ greet);
+ 
+   }
+ 
+sayHello2();
+sayHello2("Silvia");
+const retornoInancalzable = ():never => {
+  throw TypeError ("Sourcing went wrone");
+}
+const retornoInancalzable2 = (number:number):never => {
+  while(true){
+    console.log(number++);    
+  }
+}
